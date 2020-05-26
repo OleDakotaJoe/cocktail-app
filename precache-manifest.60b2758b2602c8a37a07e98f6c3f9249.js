3aa6ec8cead13cf0c08b37d05b8891f30c3c1c95
OleDakotaJoe <stevendakotasheaves@gmail.com>
2020-05-26 18:52:19 -0500

    Updates

---
+++ precache-manifest.60b2758b2602c8a37a07e98f6c3f9249.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "241dd9d63a727ba8eb8079eaa4cf4ac1",
+    "url": "/cocktail-search/index.html"
+  },
+  {
+    "revision": "965a7b7ffa1f27803440",
+    "url": "/cocktail-search/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "297e65dfb6fa44270bfb",
+    "url": "/cocktail-search/static/css/main.b1bfef81.chunk.css"
+  },
+  {
+    "revision": "965a7b7ffa1f27803440",
+    "url": "/cocktail-search/static/js/2.e71afe99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cocktail-search/static/js/2.e71afe99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "297e65dfb6fa44270bfb",
+    "url": "/cocktail-search/static/js/main.c129b264.chunk.js"
+  },
+  {
+    "revision": "fead300c55a660f9e826",
+    "url": "/cocktail-search/static/js/runtime-main.8df93218.js"
+  }
+]);
